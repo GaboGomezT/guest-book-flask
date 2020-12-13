761458b18ca23101ba1c8029cf550d27060202e1
GaboGomez09 <gomezgabriel09@gmail.com>
2020-12-13 14:44:52 -0600

    added CORS functionality

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
 from flask import Flask, request, jsonify
-# from flask_cors import CORS
-# import os
-# from dotenv import load_dotenv
-# load_dotenv()
+from flask_cors import CORS
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
-# APP_URL = os.getenv('APP_URL')
+APP_URL = os.getenv('APP_URL')
 
 app = Flask(__name__)
-# CORS(app)
+CORS(app)
 
-# cors = CORS(app, resources={
-#     r"/*":{
-#         "origin": APP_URL
-#     }
-# })
+cors = CORS(app, resources={
+    r"/*":{
+        "origin": APP_URL
+    }
+})
 messages = []
 
 @app.route('/')
