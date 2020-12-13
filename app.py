d61188598a76a9736f30d28df42ad1d8f565ae4b
GaboGomez09 <gomezgabriel09@gmail.com>
2020-12-13 14:01:54 -0600

    removed cores code

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-from flask_cors import CORS
+# from flask_cors import CORS
 import os
 from dotenv import load_dotenv
 load_dotenv()
@@ -7,13 +7,13 @@ load_dotenv()
 APP_URL = os.getenv('APP_URL')
 
 app = Flask(__name__)
-CORS(app)
+# CORS(app)
 
-cors = CORS(app, resources={
-    r"/*":{
-        "origin": APP_URL
-    }
-})
+# cors = CORS(app, resources={
+#     r"/*":{
+#         "origin": APP_URL
+#     }
+# })
 messages = []
 
 @app.route('/messages', methods = ['GET', 'POST'])
