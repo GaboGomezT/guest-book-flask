b211ed2d6f75b7f5bd7ee4a27ed8b26ff5f5d09e
GaboGomez09 <gomezgabriel09@gmail.com>
2020-12-13 15:13:06 -0600

    cleaned up file

--- app.py
+++ app.py
@@ -1,24 +1,8 @@
 from flask import Flask, request, jsonify
-from flask_cors import CORS
-import os
-from dotenv import load_dotenv
-load_dotenv()
-
-APP_URL = os.getenv('APP_URL')
 
 app = Flask(__name__)
-CORS(app)
 
-cors = CORS(app, resources={
-    r"/*":{
-        "origin": APP_URL
-    }
-})
 messages = []
-
-@app.route('/')
-def hello_world():
-    return "hello world"
 
 @app.route('/messages', methods = ['GET', 'POST'])
 def get_post():
